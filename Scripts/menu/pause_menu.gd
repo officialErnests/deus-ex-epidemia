6511c7c04746914dd76f11b730dba524ac28c328
officialErnests <ernestspursw@gmail.com>
2026-01-31 17:19:38 +0200

    When play and then exit whole game breaks

---
+++ Scripts/menu/pause_menu.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+@export var continue_button: Button
+
+func _ready() -> void:
+    visible = false
+    continue_button.connect("pressed", settingsToggle)
+
+func _input(_event: InputEvent) -> void:
+    if (Input.is_action_just_pressed("Pause")):
+        settingsToggle()
+
+func settingsToggle() -> void:
+    visible = !visible
