e6b16ab475b942d5434750dc47a28c74698b2ecc
officialErnests <ernestspursw@gmail.com>
2026-02-01 14:51:36 +0200

    cool push

--- Scripts/menu/audio_manager.gd
+++ Scripts/menu/audio_manager.gd
@@ -3,11 +3,11 @@ extends AudioStreamPlayer
 @export var sfx: bool
 
 func _ready() -> void:
-    if sfx:
-        volume_linear = settings.getSfxVolume()
-    else:
-        volume_linear = settings.getMusicVolume()
-        settings.music_volume_changed.connect(updateVol)
+	if sfx:
+		volume_linear = settings.getSfxVolume()
+	else:
+		volume_linear = settings.getMusicVolume()
+		settings.music_volume_changed.connect(updateVol)
 
 func updateVol(p_volume):
-    volume_linear = p_volume        
+	volume_linear = p_volume        
